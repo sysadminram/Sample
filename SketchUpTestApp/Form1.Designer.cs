6e141861538e4f322f5779f032438010f90838b2
sysadminram <tramkumar.engr@gmail.com>
2016-05-18 14:56:27 +0530

    Project Added

---
+++ SketchUpTestApp/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SketchUpTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxPath = new System.Windows.Forms.TextBox();
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.btnExcecute = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBoxPath
+            // 
+            this.txtBoxPath.Location = new System.Drawing.Point(66, 34);
+            this.txtBoxPath.Name = "txtBoxPath";
+            this.txtBoxPath.Size = new System.Drawing.Size(317, 20);
+            this.txtBoxPath.TabIndex = 0;
+            this.txtBoxPath.Text = "D:\\Narayanan\\Projects SVN\\Walt Disney Imagineering\\Revit SketchUp Interoperabilit" +
+    "y\\branches\\Testing\\SketchUpNET-master\\SketchUp\\SketchUpNET\\Testfiles\\TestModel.s" +
+    "kp";
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Location = new System.Drawing.Point(389, 34);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(26, 23);
+            this.btnBrowseFile.TabIndex = 1;
+            this.btnBrowseFile.Text = "...";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // btnExcecute
+            // 
+            this.btnExcecute.Location = new System.Drawing.Point(169, 79);
+            this.btnExcecute.Name = "btnExcecute";
+            this.btnExcecute.Size = new System.Drawing.Size(81, 47);
+            this.btnExcecute.TabIndex = 2;
+            this.btnExcecute.Text = "Excecute";
+            this.btnExcecute.UseVisualStyleBackColor = true;
+            this.btnExcecute.Click += new System.EventHandler(this.btnExcecute_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnExcecute);
+            this.groupBox1.Controls.Add(this.btnBrowseFile);
+            this.groupBox1.Controls.Add(this.txtBoxPath);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(430, 143);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 143);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Test Application";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxPath;
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.Button btnExcecute;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SketchUpTestApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using SketchUpNET;
+
+namespace SketchUpTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExcecute_Click(object sender, EventArgs e)
+        {
+            string filePath = txtBoxPath.Text;
+            if (!File.Exists(filePath))
+                return;
+            ExcecuteSketchUp(filePath);
+        }
+
+        private static void ExcecuteSketchUp(string filePath)
+        {
+            try
+            {
+                SketchUp skp = new SketchUp();
+                if (skp.LoadModel(filePath))
+                {
+                    List<Group> lstGroups = skp.Groups;
+                    Dictionary<string, SketchUpNET.Component> dictComponentInfo = skp.Components;
+                    List<Instance> lstInstances = skp.Instances;
+                    List<Surface> lstSurfaces = skp.Surfaces;
+
+                    for (int i = 0; i < lstSurfaces.Count; i++)
+                    {
+                        Surface surf = lstSurfaces[i];
+                        double area = surf.Area;
+                        Vector normal = surf.Normal;
+                        List<Loop> lsInnerLoops = surf.InnerEdges;
+                        Loop outerLoops = surf.OuterEdges;
+                        List<Edge> lstEdges = outerLoops.Edges;
+
+                        List<Vertex> lstVertices = surf.Vertices;
+                    }
+
+                    //foreach (Surface srf in skp.Surfaces)
+                    //    surfaces.Add(new GH_Brep(srf.ToRhinoGeo()));
+
+                    //foreach (Layer l in skp.Layers)
+                    //    layers.Add(new GH_String(l.Name));
+
+                    //foreach (Instance i in skp.Instances)
+                    //    Instances.Add(i);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.CheckFileExists = true;
+            dialog.CheckPathExists = true;
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                 txtBoxPath.Text = dialog.FileName;
+            }
+        }
+    }
+}
