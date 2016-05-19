5022e3fecd7a1d131fd753373cb23287fce19458
sysadminram <tramkumar.engr@gmail.com>
2016-05-19 11:36:15 +0530

    Update Program.cs

--- SketchUpTestApp/Program.cs
+++ SketchUpTestApp/Program.cs
@@ -16,7 +16,7 @@ namespace SketchUpTestApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run( new Form1());  
+            Application.Run(new Form1());  
         }
     }
 }
